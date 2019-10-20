616389b397a0828069b35bd6b7e9edb3dea298b9
Jiang Chunhui <jiangchunhui007@gmail.com>
2019-10-20 11:06:02 +0800

    write hashmap

--- run.py
+++ run.py
@@ -2,3 +2,18 @@ import numpy as np
 import pandas as pd
 import sklearn
 import matplotlib
+
+df = pd.read_csv("./undrg-rd1-listings/train.csv")
+
+hashmap = {}
+with open ("./undrg-rd1-listings/train.csv", "r") as f:
+    f.readline()
+    for line in f:
+        split = list(line.split(","))
+        id = int(split[0])
+        for i in range(1,len(split)):
+            name = split[i].replace("\"", "").strip()
+            hashmap[name] = id
+
+
+print(hashmap)
